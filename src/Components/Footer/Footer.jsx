f96d92dc47a69e5e4ca17f6eb1d3fe4087f93b9f
HimanshiTiwari <himanshitiwari098@gmail.com>
2025-08-09 19:56:08 +0530

    footer is added

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer.css'
+import footer_logo from '../../Assets/footer_logo.svg'
+const Footer = () => {
+  return (
+    <div>
+      <div className="footer">
+            <div className="footer-content">
+                <p>© 2023 Himanshi Tiwari. All rights reserved.</p>
+            </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
