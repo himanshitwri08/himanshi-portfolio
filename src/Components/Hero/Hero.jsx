3580b6fe2572313e855d5c5d2a0cfff1575dd839
HimanshiTiwari <himanshitiwari098@gmail.com>
2025-08-14 22:35:11 +0530

    hero section edited

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Hero.css'
-import profile_img from '../../assets/profile_img.svg'
+import profile from '../../assets/profile.png'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 const Hero = () => {
   return (
-    <div className='hero'>
-     <img src={profile_img} alt="Profile" />
-     <h1><span>I'm Himanshi Tiwari</span>, full stack developer</h1>
+    <div id="home" className='hero'>
+     <img src={profile} alt="Profile" />
+     <h1><span>I'm Himanshi Tiwari</span>, frontend developer</h1>
      <p>I will be writing something here</p>
      <div className="hero-action">
-      <div className="hero-connect">Connect with me</div>
+      <div className="hero-connect"><AnchorLink className="anchor-link" href="#contact" onClick={()=> setMenu("#contact")}>Connect with me</AnchorLink></div>
       <div className="hero-resume">My resume</div>
      </div>
     </div>
