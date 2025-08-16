0eb73ac6ab4163403b0ee04c372baa8fc3294d1c
HimanshiTiwari <himanshitiwari098@gmail.com>
2025-08-16 13:17:07 +0530

    project section edited

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -4,7 +4,7 @@ import theme_pattern from '../../assets/theme_pattern.svg'
 import mywork_data from '../../assets/mywork_data'
 const Project = () => {
   return (
-    <div className='projects'>
+    <div id="project"className='projects'>
        <div className="project-title">
           <h1>My Projects</h1>
           <img src={theme_pattern} alt="theme_pattern" />
