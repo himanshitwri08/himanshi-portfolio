dfcb5fd36a589e1713b8f6bd596e289bf641bb8c
HimanshiTiwari <himanshitiwari098@gmail.com>
2025-08-16 13:15:30 +0530

    edited

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div>
       <div className="footer">
             <div className="footer-content">
-                <p>© 2023 Himanshi Tiwari. All rights reserved.</p>
+                <p>© 2025 Himanshi Tiwari. All rights reserved.</p>
             </div>
       </div>
     </div>
