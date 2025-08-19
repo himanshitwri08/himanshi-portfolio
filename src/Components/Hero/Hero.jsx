703199f62e46e216133f3db1aa39229a3caafb4f
HimanshiTiwari <himanshitiwari098@gmail.com>
2025-08-19 16:05:11 +0530

    edit the mob version

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,16 +2,30 @@ import React from 'react'
 import './Hero.css'
 import profile from '../../assets/profile.png'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+
 const Hero = () => {
   return (
     <div id="home" className='hero'>
-     <img src={profile} alt="Profile" />
-     <h1><span>I'm Himanshi Tiwari</span>, frontend developer</h1>
-     <p>I will be writing something here</p>
-     <div className="hero-action">
-      <div className="hero-connect"><AnchorLink className="anchor-link" href="#contact" onClick={()=> setMenu("#contact")}>Connect with me</AnchorLink></div>
-      <div className="hero-resume">My resume</div>
-     </div>
+      <img src={profile} alt="Profile" />
+      <h1><span>I'm Himanshi Tiwari</span>, frontend developer</h1>
+      <p>I will be writing something here</p>
+      <div className="connect-icons">
+        
+        <a href="https://www.linkedin.com/in/himanshi-tiwari810" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} style={{ color: "#fff", fontSize: "30px" }} />
+        </a>
+
+        <a href="https://github.com/himanshitwri08" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faGithub} style={{ color: "#fff", fontSize: "30px", marginLeft: "15px" }} />
+        </a>
+      </div>
+
+      <div className="hero-action">
+        <div className="hero-connect"><AnchorLink className="anchor-link" href="#contact" onClick={() => setMenu("#contact")}>Connect with me</AnchorLink></div>
+        <div className="hero-resume">My resume</div>
+      </div>
     </div>
   )
 }
